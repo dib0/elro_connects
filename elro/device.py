501cf31b53132d232274ebb2aa2c45301162130c
Johannes Kulick <kulick@hildensia.de>
2021-02-19 22:25:33 +0100

    Let the device send events
    Problem:
    We want to notify consumers of events in the device.
    
    Solution:
    Add events to the device, that fire accordingly.

--- elro/device.py
+++ elro/device.py
@@ -1,5 +1,5 @@
-from dataclasses import dataclass
 from enum import Enum
+import trio
 
 
 class DeviceType(Enum):
@@ -10,13 +10,41 @@ class DeviceType(Enum):
     DOOR_WINDOW_SENSOR = "0101"
 
 
-@dataclass
 class Device:
-    id: str
-    name: str
-    battery_level: int
-    device_state: str
-    device_type: DeviceType
+    def __init__(self, id, name, battery_level, device_state, device_type):
+        self.id = id
+        self.name = name
+        self._battery_level = battery_level
+        self._device_state = device_state
+        self.device_type = device_type
+        self.updated = trio.Event()
+        self.alarm = trio.Event()
+
+    @property
+    def device_state(self):
+        return self._device_state
+
+    @device_state.setter
+    def device_state(self, device_state):
+        self._device_state = device_state
+        self._send_update_event()
+
+    @property
+    def battery_level(self):
+        return self._battery_level
+
+    @battery_level.setter
+    def battery_level(self, battery_level):
+        self._battery_level = battery_level
+        self._send_update_event()
+
+    def _send_update_event(self):
+        self.updated.set()
+        self.updated = trio.Event()
+
+    def send_alarm_event(self):
+        self.alarm.set()
+        self.alarm = trio.Event()
 
 
 class DeviceDict(dict):
@@ -26,5 +54,5 @@ class DeviceDict(dict):
         super().__init__(seq, **kwargs)
 
     def __missing__(self, key):
-        self[key] = Device(key, "", 100, "", None)
+        self[key] = Device(key, "", 100, "", None, trio.Event())
         return self[key]
